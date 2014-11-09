10b5d158ac33e55ab87e8c077adeb80385484a59
Finn Christiansen <kontakt@finnchristiansen.de>
2014-11-09 13:29:19 +0100

    added warning and critical line for check_dht pnp graph

--- check_dht/check_dht.php
+++ check_dht/check_dht.php
@@ -1,6 +1,33 @@
 <?php
 $vertlabel = "Temperature and Humidity";
 
+$temp_warning = '';
+$hum_warning = '';
+$temp_critical = '';
+$hum_critical = '';
+
+$_WARNRULE = '#FFFF00';
+$_CRITRULE = '#FF0000';
+
+
+if ( $WARN[1] != '' )
+{
+	$temp_warning = $WARN[1];
+}
+if ( $CRIT[1] != '' )
+{
+	$temp_critical = $CRIT[1];
+}
+
+if ( $WARN[2] != '' )
+{
+	$hum_warning = $WARN[2];
+}
+if ( $CRIT[2] != '' )
+{
+	$hum_critical = $CRIT[2];
+}
+
 $opt[1] = '';
 $def[1] = '';
 
@@ -16,6 +43,14 @@ $def[1] .= "GPRINT:temp:AVERAGE:\"Avg\\:%8.2lf $UNIT[1]\" ";
 $def[1] .= "GPRINT:temp:MIN:\"Min\\:%8.2lf $UNIT[1]\" ";
 $def[1] .= "GPRINT:temp:MAX:\"Max\\:%8.2lf $UNIT[1]\\n\" ";
 
+if ($temp_warning != "") {
+	$def[1] .= rrd::hrule($temp_warning, $_WARNRULE, "Warning  $temp_warning \\n");
+}
+if ($temp_critical != "") {
+	$def[1] .= rrd::hrule($temp_critical, $_CRITRULE, "Critical  $temp_critical \\n");
+}
+
+
 $opt[2] = '';
 $def[2] = '';
 
@@ -30,4 +65,12 @@ $def[2] .= "GPRINT:hum:LAST:\"Cur\\:%8.2lf $UNIT[2]\" ";
 $def[2] .= "GPRINT:hum:AVERAGE:\"Avg\\:%8.2lf $UNIT[2]\" ";
 $def[2] .= "GPRINT:hum:MIN:\"Min\\:%8.2lf $UNIT[2]\" ";
 $def[2] .= "GPRINT:hum:MAX:\"Max\\:%8.2lf $UNIT[2]\\n\" ";
+
+if ($hum_warning != "") {
+	$def[2] .= rrd::hrule($hum_warning, $_WARNRULE, "Warning  $hum_warning \\n");
+}
+if ($hum_critical != "") {
+	$def[2] .= rrd::hrule($hum_critical, $_CRITRULE, "Critical  $hum_critical \\n");
+}
+
 ?>

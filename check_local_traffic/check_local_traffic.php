746c0531beb1b91028ef05c00ebb90eba4ba193f
Finn Christiansen <kontakt@finnchristiansen.de>
2014-11-09 11:20:57 +0100

    changed pnp4nagios template for check_local_traffic

--- check_local_traffic/check_local_traffic.php
+++ check_local_traffic/check_local_traffic.php
@@ -22,7 +22,7 @@ foreach ($DS as $i)
 		$def[$defcnt] .= rrd::def("in", $RRDFILE[$defcnt], $DS[$defcnt], "AVERAGE"); 
 		$def[$defcnt] .= rrd::area("in", $colors['in_area']);
 		$def[$defcnt] .= rrd::line1("in", $colors['in'], "in") ;
-		$def[$defcnt] .= rrd::gprint("in", array("LAST", "MIN", "MAX", "AVERAGE"), "%4.2lf $UNIT[$defcnt]") ;
+                $def[$defcnt] .= rrd::gprint("in", array("LAST", "MIN", "MAX", "AVERAGE"), "%4.2lf %s");
 		
 		$defcntout = 1;
 		foreach ($DS as $j)
@@ -32,8 +32,8 @@ foreach ($DS as $i)
 				$def[$defcnt] .= rrd::def("out", $RRDFILE[$defcntout], $DS[$defcntout], "AVERAGE"); 
 				$def[$defcnt] .= rrd::cdef("out_neg", "out,-1,*");
 				$def[$defcnt] .= rrd::area("out_neg", $colors['out_area']);
-				$def[$defcnt] .= rrd::line1("out_neg", $colors['out'], "out") ;
-				$def[$defcnt] .= rrd::gprint("out", array("LAST", "MIN", "MAX", "AVERAGE"), "%4.2lf $UNIT[$defcntout]") ;
+                                $def[$defcnt] .= rrd::line1("out_neg", $colors['out'], "out") ;
+                                $def[$defcnt] .= rrd::gprint("out", array("LAST", "MIN", "MAX", "AVERAGE"), "%4.2lf %s");
 			}
         		$defcntout++;
 		}

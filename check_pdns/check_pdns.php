0b9769509d4689ce105d0108af8879cdcb1192de
Finn Christiansen <kontakt@finnchristiansen.de>
2014-12-28 10:28:48 +0100

    changed colors for check_pdns template

--- check_pdns/check_pdns.php
+++ check_pdns/check_pdns.php
@@ -67,19 +67,19 @@ $def[3] .= "CDEF:packetcache-hit=ds5 ";
 $def[3] .= "CDEF:packetcache-miss=ds11 ";
 
 $ds_name[3] = 'Cache';
-$def[3] .= "LINE1:query-cache-hit" . "#009900" . "FF:\"$NAME[3]\t\" " ;
+$def[3] .= "LINE1:query-cache-hit" . "#006600" . "FF:\"$NAME[3]\t\" " ;
 $def[3] .= "GPRINT:query-cache-hit:LAST:\"Cur\\:%8.2lf $UNIT[3]\" ";
 $def[3] .= "GPRINT:query-cache-hit:AVERAGE:\"Avg\\:%8.2lf $UNIT[3]\" ";
 $def[3] .= "GPRINT:query-cache-hit:MIN:\"Min\\:%8.2lf $UNIT[3]\" ";
 $def[3] .= "GPRINT:query-cache-hit:MAX:\"Max\\:%8.2lf $UNIT[3]\\n\" ";
 
-$def[3] .= "LINE1:query-cache-miss" . "#99FF99" . "FF:\"$NAME[8]\t\" " ;
+$def[3] .= "LINE1:query-cache-miss" . "#00FF00" . "FF:\"$NAME[8]\t\" " ;
 $def[3] .= "GPRINT:query-cache-miss:LAST:\"Cur\\:%8.2lf $UNIT[8]\" ";
 $def[3] .= "GPRINT:query-cache-miss:AVERAGE:\"Avg\\:%8.2lf $UNIT[8]\" ";
 $def[3] .= "GPRINT:query-cache-miss:MIN:\"Min\\:%8.2lf $UNIT[8]\" ";
 $def[3] .= "GPRINT:query-cache-miss:MAX:\"Max\\:%8.2lf $UNIT[8]\\n\" ";
 
-$def[3] .= "LINE1:packetcache-hit" . "#000099" . "FF:\"$NAME[5]\t\" " ;
+$def[3] .= "LINE1:packetcache-hit" . "#000066" . "FF:\"$NAME[5]\t\" " ;
 $def[3] .= "GPRINT:packetcache-hit:LAST:\"Cur\\:%8.2lf $UNIT[5]\" ";
 $def[3] .= "GPRINT:packetcache-hit:AVERAGE:\"Avg\\:%8.2lf $UNIT[5]\" ";
 $def[3] .= "GPRINT:packetcache-hit:MIN:\"Min\\:%8.2lf $UNIT[5]\" ";
@@ -101,13 +101,13 @@ $def[4] .= "CDEF:recursing-answers=ds4 ";
 $def[4] .= "CDEF:recursing-questions=ds6 ";
 
 $ds_name[4] = 'Recursor';
-$def[4] .= "LINE1:recursing-questions" . "#990000" . "FF:\"".substr_replace($NAME[6], '', 17)."\t\" " ;
+$def[4] .= "LINE1:recursing-questions" . "#660000" . "FF:\"".substr_replace($NAME[6], '', 17)."\t\" " ;
 $def[4] .= "GPRINT:recursing-questions:LAST:\"Cur\\:%8.2lf $UNIT[6]\" ";
 $def[4] .= "GPRINT:recursing-questions:AVERAGE:\"Avg\\:%8.2lf $UNIT[6]\" ";
 $def[4] .= "GPRINT:recursing-questions:MIN:\"Min\\:%8.2lf $UNIT[6]\" ";
 $def[4] .= "GPRINT:recursing-questions:MAX:\"Max\\:%8.2lf $UNIT[6]\\n\" ";
 
-$def[4] .= "LINE1:recursing-answers" . "#FF9999" . "FF:\"$NAME[4]\t\" " ;
+$def[4] .= "LINE1:recursing-answers" . "#FF0000" . "FF:\"$NAME[4]\t\" " ;
 $def[4] .= "GPRINT:recursing-answers:LAST:\"Cur\\:%8.2lf $UNIT[4]\" ";
 $def[4] .= "GPRINT:recursing-answers:AVERAGE:\"Avg\\:%8.2lf $UNIT[4]\" ";
 $def[4] .= "GPRINT:recursing-answers:MIN:\"Min\\:%8.2lf $UNIT[4]\" ";

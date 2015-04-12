e386ca0e97d515059b531e0d96442ba40831d8fe
Finn Christiansen <kontakt@finnchristiansen.de>
2015-04-12 12:20:55 +0200

    Clean up check to keep it simple, removed unnecessary class and code is now pep8 conform.

--- check_local_traffic/check_local_traffic.py
+++ check_local_traffic/check_local_traffic.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """
-    check_python.py - simple nagios plugin to check traffic on linux hosts
+    check_local_traffic.py - simple nagios plugin to check traffic on linux
+    hosts
     Copyright (C) 2012 Finn Christiansen
 
     This program is free software: you can redistribute it and/or modify
@@ -17,23 +18,12 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 This is a small nagios plugin used for checking or watching the traffic on
-linux host by reading /sys/class/net/<device>/statistics/* and storing 
-information in a file to calculate the average and total traffic. 
-Currently it can be only used to get performance data, warning and critial 
+linux host by reading /sys/class/net/<device>/statistics/* and storing
+information in a file to calculate the average and total traffic.
+Currently it can be only used to get performance data, warning and critial
 values will be added soon.
 
 """
-
-
-__author__ = "Finn Christiansen"
-__copyright__ = "Copyright 2012-2013, Finn Christiansen"
-__credits__ = ["Finn Christiansen"]
-__license__ = "GPL"
-__version__ = "3"
-__maintainer__ = "Finn Christiansen"
-__email__ = "kontakt@finnchristiansen.de"
-__status__ = "Development"
-
 
 import sys
 import re
@@ -44,97 +34,98 @@ import argparse
 import math
 
 
-class PluginData:
-	def __init__(self, device):
-		self.storePath = '/tmp/check_local_traffic_%s' % device
-		self.statsPath = '/sys/class/net/%s/statistics/' % device
-		self.values = ['collisions', 'multicast', 'rx_bytes', 'rx_compressed', 'rx_crc_errors', 'rx_dropped', 'rx_errors', 'rx_fifo_errors', 'rx_frame_errors', 'rx_length_errors', 'rx_missed_errors', 'rx_over_errors', 'rx_packets', 'tx_aborted_errors', 'tx_bytes', 'tx_carrier_errors', 'tx_compressed', 'tx_dropped', 'tx_errors', 'tx_fifo_errors', 'tx_heartbeat_errors', 'tx_packets', 'tx_window_errors']
-		if not os.path.isfile(self.storePath):
-			self.getData()
-			self.setData()
-
-	def initData(self):
-		self.data = dict()
-		for key in self.values:
-			self.data[key] = int(open('%s%s' % (self.statsPath, key)).read())
-		self.data['timeLastCheck'] = time.time() 
-		
-	def getData(self):
-		self.data = dict()
-		try:
-			for key in self.values:
-				self.data[key] = int(open('%s%s' % (self.statsPath, key)).read())
-		except IOError as e:
-			return False	
-		self.data['timeLastCheck'] = time.time() 
-		return self.data
-	
-	def setData(self):
-		pluginStore = shelve.open(self.storePath)
-		pluginStore['pluginData'] = self.data
-		pluginStore.close()
-		return True
-
-	def getStoredData(self):
-		pluginDataFile = shelve.open(self.storePath)
-		pluginData = pluginDataFile['pluginData']
-		pluginDataFile.close()
-		return pluginData
-
-def main():
-	parser = argparse.ArgumentParser(description = 'Nagios plugin to check traffic usage')
-	parser.add_argument('-d', '--device', required=True)
-	args = parser.parse_args()
-	if args.device is not None:
-		returnCode = doCheck(args.device)
-	else:
-		returnCode = 3
-
-	return returnCode
+storePath = '/tmp/check_local_traffic_%s'
+statsPath = '/sys/class/net/%s/statistics/'
 
 
-def processData(device):
-	trafficData = PluginData(device)
-	trafficDataOld = trafficData.getStoredData()
-	trafficDataNew = trafficData.getData()
-	if trafficDataNew is False:
-		return False
+def getCurrentData(device):
+    values = [
+        'collisions', 'multicast', 'rx_bytes', 'rx_compressed',
+        'rx_crc_errors', 'rx_dropped', 'rx_errors', 'rx_fifo_errors',
+        'rx_frame_errors', 'rx_length_errors', 'rx_missed_errors',
+        'rx_over_errors', 'rx_packets', 'tx_aborted_errors', 'tx_bytes',
+        'tx_carrier_errors', 'tx_compressed', 'tx_dropped', 'tx_errors',
+        'tx_fifo_errors', 'tx_heartbeat_errors', 'tx_packets',
+        'tx_window_errors'
+        ]
 
-	deltaTime = time.time() - trafficDataOld['timeLastCheck']
-	trafficDataOutput = dict()
+    data = dict()
+    for key in values:
+        data[key] = int(open('%s%s' % (statsPath % device, key)).read())
+    data['timeLastCheck'] = time.time()
 
-	# if last value seems incorrect... can happen after reboot
-	if	trafficDataOld['rx_bytes'] > trafficDataNew['rx_bytes'] or \
-		trafficDataOld['tx_bytes'] > trafficDataNew['tx_bytes'] or \
-		trafficDataOld['rx_bytes'] == 0 or \
-		trafficDataOld['tx_bytes'] == 0:
-		for key in trafficDataNew:
-			trafficDataOutput[key] = 0
-	else:
-		for key in trafficDataNew:
-			trafficDataOutput[key] = (int(trafficDataNew[key]) - int(trafficDataOld[key])) / deltaTime
+    if not os.path.isfile(storePath % device):
+        data = getCurrentData(device)
+        setData(device, data)
+    data = dict()
+    try:
+        for key in values:
+            data[key] = int(open('%s%s' % (statsPath % device, key)).read())
+    except IOError as e:
+        return False
+    data['timeLastCheck'] = time.time()
+    return data
 
-	trafficData.setData()
-	return trafficDataOutput
 
+def setData(device, data):
+    pluginStore = shelve.open(storePath % device)
+    pluginStore['pluginData'] = data
+    pluginStore.close()
+    return True
+
+
+def getStoredData(device):
+    try:
+        pluginDataFile = shelve.open(storePath % device)
+        pluginData = pluginDataFile['pluginData']
+        pluginDataFile.close()
+    except:
+        pluginData = getCurrentData(device)
+    return pluginData
 
 
 def doCheck(device):
-	trafficData = processData(device)
+    if os.path.isdir(statsPath % device):
+        trafficDataOld = getStoredData(device)
+        trafficDataNew = getCurrentData(device)
+        if trafficDataNew is False:
+            return False
 
-	if trafficData is not False:
-		perfdataString  = "bits_in/s={0:.3f};".format(trafficData['rx_bytes']*8)
-		perfdataString += " bits_out/s={0:.3f};".format(trafficData['tx_bytes']*8)
-                textOutput = "OK - %s in: %s - out: %s | %s" % (device,
-                        formatBits(trafficData['rx_bytes']),
-                        formatBits(trafficData['tx_bytes']), perfdataString)
-		returnCode = 0
-	else:
-		textOutput = 'UNKNOWN - device ' + device + ' not found'
-		returnCode = 3
+        deltaTime = time.time() - trafficDataOld['timeLastCheck']
+        trafficData = dict()
 
-	print(textOutput)
-	return returnCode
+        # if last value seems incorrect... can happen after reboot
+        if (trafficDataOld['rx_bytes'] > trafficDataNew['rx_bytes'] or
+                trafficDataOld['tx_bytes'] > trafficDataNew['tx_bytes'] or
+                trafficDataOld['rx_bytes'] == 0 or
+                trafficDataOld['tx_bytes'] == 0):
+            for key in trafficDataNew:
+                trafficData[key] = 0
+        else:
+            for key in trafficDataNew:
+                trafficData[key] = (int(trafficDataNew[key]) -
+                                    int(trafficDataOld[key])) / deltaTime
+
+        setData(device, trafficDataNew)
+
+        if trafficData is not False:
+            perfdataString = "bits_in/s={0:.3f};". \
+            format(trafficData['rx_bytes']*8)
+            perfdataString += " bits_out/s={0:.3f};". \
+            format(trafficData['tx_bytes']*8)
+            textOutput = "OK - %s in: %s - out: %s | %s" % \
+            (device, formatBits(trafficData['rx_bytes']),
+                formatBits(trafficData['tx_bytes']), perfdataString)
+            returnCode = 0
+        else:
+            textOutput = 'UNKNOWN - device ' + device + ' not found'
+            returnCode = 3
+    else:
+        textOutput = 'UNKNOWN - device ' + device + ' not found'
+        returnCode = 3
+    print(textOutput)
+    return returnCode
+
 
 def formatBits(value):
     output = value * 8
@@ -146,5 +137,24 @@ def formatBits(value):
     return "{0:.3f};".format(output) + " " + size_names[i]
 
 
+def deviceExists(device):
+    exists = False
+    if os.path.isfile(statsPath % device):
+        exists = False
+    return exists
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Nagios plugin to check \
+            traffic usage')
+    parser.add_argument('-d', '--device', required=True)
+    args = parser.parse_args()
+    if args.device is not None:
+        returnCode = doCheck(args.device)
+    else:
+        returnCode = 3
+
+    return returnCode
+
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())

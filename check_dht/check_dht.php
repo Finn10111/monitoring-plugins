19ba6325283589ce2ac3b3bcdf776e3f65cb3efa
Finn Christiansen <kontakt@finnchristiansen.de>
2015-05-10 16:48:20 +0200

    Added support for warning/critical thresholds like 10:20 for check_dht PNP4Nagios template

--- check_dht/check_dht.php
+++ check_dht/check_dht.php
@@ -9,25 +9,6 @@ $hum_critical = '';
 $_WARNRULE = '#FFFF00';
 $_CRITRULE = '#FF0000';
 
-
-if ( $WARN[1] != '' )
-{
-	$temp_warning = $WARN[1];
-}
-if ( $CRIT[1] != '' )
-{
-	$temp_critical = $CRIT[1];
-}
-
-if ( $WARN[2] != '' )
-{
-	$hum_warning = $WARN[2];
-}
-if ( $CRIT[2] != '' )
-{
-	$hum_critical = $CRIT[2];
-}
-
 $opt[1] = '';
 $def[1] = '';
 
@@ -42,14 +23,6 @@ $def[1] .= "GPRINT:temp:LAST:\"Cur\\:%8.2lf $UNIT[1]\" ";
 $def[1] .= "GPRINT:temp:AVERAGE:\"Avg\\:%8.2lf $UNIT[1]\" ";
 $def[1] .= "GPRINT:temp:MIN:\"Min\\:%8.2lf $UNIT[1]\" ";
 $def[1] .= "GPRINT:temp:MAX:\"Max\\:%8.2lf $UNIT[1]\\n\" ";
-
-if ($temp_warning != "") {
-	$def[1] .= rrd::hrule($temp_warning, $_WARNRULE, "Warning  $temp_warning \\n");
-}
-if ($temp_critical != "") {
-	$def[1] .= rrd::hrule($temp_critical, $_CRITRULE, "Critical  $temp_critical \\n");
-}
-
 
 $opt[2] = '';
 $def[2] = '';
@@ -66,11 +39,55 @@ $def[2] .= "GPRINT:hum:AVERAGE:\"Avg\\:%8.2lf $UNIT[2]\" ";
 $def[2] .= "GPRINT:hum:MIN:\"Min\\:%8.2lf $UNIT[2]\" ";
 $def[2] .= "GPRINT:hum:MAX:\"Max\\:%8.2lf $UNIT[2]\\n\" ";
 
-if ($hum_warning != "") {
+if ( $WARN[1] != '' )
+{
+	$temp_warning = $WARN[1];
+	$def[1] .= rrd::hrule($temp_warning, $_WARNRULE, "Warning  $temp_warning \\n");
+}
+else if ( $WARN_MIN[1] != '' )
+{
+	$temp_warning_min = $WARN_MIN[1];
+	$temp_warning_max = $WARN_MAX[1];
+	$def[1] .= rrd::hrule($temp_warning_min, $_WARNRULE, "Warning (min)  $temp_warning_min \\n");
+	$def[1] .= rrd::hrule($temp_warning_max, $_WARNRULE, "Warning (max)  $temp_warning_max \\n");
+}
+
+if ( $CRIT[1] != '' )
+{
+	$temp_critical = $CRIT[1];
+	$def[1] .= rrd::hrule($temp_critical, $_CRITRULE, "Critical  $temp_critical \\n");
+}
+else if ( $CRIT_MIN[1] != '' )
+{
+	$temp_critical_min = $CRIT_MIN[1];
+	$temp_critical_max = $CRIT_MAX[1];
+	$def[1] .= rrd::hrule($temp_critical_min, $_CRITRULE, "Critical (min)  $temp_critical_min \\n");
+	$def[1] .= rrd::hrule($temp_critical_max, $_CRITRULE, "Critical (max)  $temp_critical_max \\n");
+}
+
+if ( $WARN[2] != '' )
+{
+	$hum_warning = $WARN[2];
 	$def[2] .= rrd::hrule($hum_warning, $_WARNRULE, "Warning  $hum_warning \\n");
 }
-if ($hum_critical != "") {
+else if ( $WARN_MIN[2] != '' )
+{
+	$hum_warning_min = $WARN_MIN[2];
+	$hum_warning_max = $WARN_MAX[2];
+	$def[2] .= rrd::hrule($hum_warning_min, $_WARNRULE, "Warning (min)  $hum_warning_min \\n");
+	$def[2] .= rrd::hrule($hum_warning_max, $_WARNRULE, "Warning (max)  $hum_warning_max \\n");
+}
+if ( $CRIT[2] != '' )
+{
+	$hum_critical = $CRIT[2];
 	$def[2] .= rrd::hrule($hum_critical, $_CRITRULE, "Critical  $hum_critical \\n");
+}
+else if ( $CRIT_MIN[2] != '' )
+{
+	$hum_critical_min = $CRIT_MIN[2];
+	$hum_critical_max = $CRIT_MAX[2];
+	$def[2] .= rrd::hrule($hum_critical_min, $_CRITRULE, "Critical (min)  $hum_critical_min \\n");
+	$def[2] .= rrd::hrule($hum_critical_max, $_CRITRULE, "Critical (max)  $hum_critical_max \\n");
 }
 
 ?>

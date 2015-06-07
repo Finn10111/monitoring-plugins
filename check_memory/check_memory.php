1670abe1c01f4a61f57b13d9fa5a0147566aeec8
Finn Christiansen <kontakt@finnchristiansen.de>
2015-06-07 12:35:03 +0200

    Simplified check_memory.php PNP4Nagios template by using template helpers

--- check_memory/check_memory.php
+++ check_memory/check_memory.php
@@ -7,53 +7,28 @@ $def[1] = '';
  
 $opt[1] .= " --imgformat=PNG --title=\" $hostname / " . $this->MACRO['DISP_SERVICEDESC'] . "\" --base=1024 --vertical-label=\"$vertlabel\" --slope-mode -l 0 ";
  
-$def[1] .= "DEF:ds1=$RRDFILE[1]:$DS[1]:AVERAGE " ;
-$def[1] .= "DEF:ds2=$RRDFILE[2]:$DS[2]:AVERAGE " ;
-$def[1] .= "DEF:ds3=$RRDFILE[3]:$DS[3]:AVERAGE " ;
-$def[1] .= "DEF:ds4=$RRDFILE[4]:$DS[4]:AVERAGE " ;
-$def[1] .= "DEF:ds5=$RRDFILE[5]:$DS[5]:AVERAGE " ;
-$def[1] .= "DEF:ds6=$RRDFILE[6]:$DS[6]:AVERAGE " ;
-$def[1] .= "DEF:ds7=$RRDFILE[7]:$DS[7]:AVERAGE " ;
-$def[1] .= "CDEF:total=ds1 ";
-$def[1] .= "CDEF:free=ds2 ";
-$def[1] .= "CDEF:totalfree=ds3 ";
-$def[1] .= "CDEF:used=ds4 ";
-$def[1] .= "CDEF:buffers=ds5 ";
-$def[1] .= "CDEF:cached=ds6 ";
-$def[1] .= "CDEF:swapcached=ds7 ";
+$def[1] .= rrd::def('total', $RRDFILE[1], $DS[1], 'AVERAGE');
+$def[1] .= rrd::def('free', $RRDFILE[2], $DS[2], 'AVERAGE');
+$def[1] .= rrd::def('total_free', $RRDFILE[3], $DS[3], 'AVERAGE');
+$def[1] .= rrd::def('used', $RRDFILE[4], $DS[4], 'AVERAGE');
+$def[1] .= rrd::def('buffers', $RRDFILE[5], $DS[5], 'AVERAGE');
+$def[1] .= rrd::def('cached', $RRDFILE[6], $DS[6], 'AVERAGE');
+$def[1] .= rrd::def('swapcached', $RRDFILE[7], $DS[7], 'AVERAGE');
 
+$def[1] .= rrd::area('used', '#FF0000', 'used    ' );
+$def[1] .= rrd::gprint('used', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%6.2lf %s' );
+ 
+$def[1] .= rrd::area('buffers', '#7700FF', 'buffers ', true );
+$def[1] .= rrd::gprint('buffers', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%6.2lf %s' );
 
-$def[1] .= "AREA:used" . "#FF0000" . "FF:\"$NAME[4]\t\" " ;
-$def[1] .= "GPRINT:used:LAST:\"Cur\\:%8.2lf $UNIT[4]\" ";
-$def[1] .= "GPRINT:used:AVERAGE:\"Avg\\:%8.2lf $UNIT[4]\" ";
-$def[1] .= "GPRINT:used:MAX:\"Max\\:%8.2lf $UNIT[4]\\n\" ";
- 
-$def[1] .= "AREA:buffers" . "#7700FF" . "FF:\"$NAME[5]\t\":STACK " ;
-$def[1] .= "GPRINT:buffers:LAST:\"Cur\\:%8.2lf $UNIT[5]\" ";
-$def[1] .= "GPRINT:buffers:AVERAGE:\"Avg\\:%8.2lf $UNIT[5]\" ";
-$def[1] .= "GPRINT:buffers:MAX:\"Max\\:%8.2lf $UNIT[5]\\n\" ";
+$def[1] .= rrd::area('cached', '#33BBFF', 'cached  ', true );
+$def[1] .= rrd::gprint('cached', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%6.2lf %s' );
 
-$def[1] .= "AREA:cached" . "#33BBFF" . "FF:\"$NAME[6]\t\":STACK " ;
-$def[1] .= "GPRINT:cached:LAST:\"Cur\\:%8.2lf $UNIT[6]\" ";
-$def[1] .= "GPRINT:cached:AVERAGE:\"Avg\\:%8.2lf $UNIT[6]\" ";
-$def[1] .= "GPRINT:cached:MAX:\"Max\\:%8.2lf $UNIT[6]\\n\" ";
-
-
-
-$def[1] .= "AREA:free" . "#00FF00" . "FF:\"$NAME[2]\t\":STACK " ;
-$def[1] .= "GPRINT:free:LAST:\"Cur\\:%8.2lf $UNIT[2]\" ";
-$def[1] .= "GPRINT:free:AVERAGE:\"Avg\\:%8.2lf $UNIT[2]\" ";
-$def[1] .= "GPRINT:free:MAX:\"Max\\:%8.2lf $UNIT[2]\\n\" ";
+$def[1] .= rrd::area('free', '#00FF00', 'free    ', true );
+$def[1] .= rrd::gprint('free', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%6.2lf %s' );
  
- 
- 
- 
- 
-$def[1] .= "LINE1:total" . "#000000" . "FF:\"$NAME[1]\t\" ";
-$def[1] .= "GPRINT:total:LAST:\"Cur\\:%8.2lf $UNIT[1]\" ";
-$def[1] .= "GPRINT:total:AVERAGE:\"Avg\\:%8.2lf $UNIT[1]\" ";
-$def[1] .= "GPRINT:total:MAX:\"Max\\:%8.2lf $UNIT[1]\\n\" ";
- 
+$def[1] .= rrd::line1('total', '#000000', 'total   ');
+$def[1] .= rrd::gprint('total', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%6.2lf %s' );
  
 # warning and critial thresholds not implemented in check yet
  

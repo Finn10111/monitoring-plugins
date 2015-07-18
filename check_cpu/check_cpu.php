5a054c80efcb7ae27f8eec8716a478c2334c1446
Finn Christiansen <kontakt@finnchristiansen.de>
2015-07-18 19:31:31 +0200

    Updated check_cpu PNP4Nagios template.

--- check_cpu/check_cpu.php
+++ check_cpu/check_cpu.php
@@ -5,7 +5,7 @@ $vertlabel = "CPU";
 $opt[1] = '';
 $def[1] = '';
  
-$opt[1] .= " --imgformat=PNG --title=\" $hostname / " . $this->MACRO['DISP_SERVICEDESC'] . "\" --base=1024 --upper-limit=100 --vertical-label=\"$vertlabel\" --slope-mode -l 0 ";
+$opt[1] .= " --imgformat=PNG --title=\" $hostname / " . $this->MACRO['DISP_SERVICEDESC'] . "\" --base=1000 --vertical-label=\"$vertlabel\" --slope-mode -l 0 -u 100 --rigid ";
 $def[1]	.= rrd::def('user', $RRDFILE[1], $DS[1], 'AVERAGE');
 $def[1]	.= rrd::def('nice', $RRDFILE[2], $DS[2], 'AVERAGE');
 $def[1]	.= rrd::def('system', $RRDFILE[3], $DS[3], 'AVERAGE');
@@ -14,24 +14,24 @@ $def[1]	.= rrd::def('iowait', $RRDFILE[5], $DS[5], 'AVERAGE');
 $def[1]	.= rrd::def('irq', $RRDFILE[6], $DS[6], 'AVERAGE');
 $def[1]	.= rrd::def('softirq', $RRDFILE[7], $DS[7], 'AVERAGE');
 
-$def[1] .= rrd::area('user', '#ff6e7e', 'user    ', true );
-$def[1] .= rrd::gprint('user', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
-
-$def[1] .= rrd::area('nice', '#7fff6e', 'nice    ', true );
-$def[1] .= rrd::gprint('nice', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
-
-$def[1] .= rrd::area('system', '#6e7fff', 'system  ', true );
+$def[1] .= rrd::area('system', '#9f40ff', 'system  ', true );
 $def[1] .= rrd::gprint('system', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
 
-$def[1] .= rrd::area('iowait', '#b83140', 'iowait  ', true );
+$def[1] .= rrd::area('user', '#40ffff', 'user    ', true );
+$def[1] .= rrd::gprint('user', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
+
+$def[1] .= rrd::area('nice', '#9fff40', 'nice    ', true );
+$def[1] .= rrd::gprint('nice', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
+
+$def[1] .= rrd::area('iowait', '#ff4040', 'iowait  ', true );
 $def[1] .= rrd::gprint('iowait', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
 
-$def[1] .= rrd::area('irq', '#41b832', 'irq     ', true );
+$def[1] .= rrd::area('irq', '#ffc440', 'irq     ', true );
 $def[1] .= rrd::gprint('irq', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
 
-$def[1] .= rrd::area('softirq', '#3241b8', 'softirq ', true );
+$def[1] .= rrd::area('softirq', '#ff40dc', 'softirq ', true );
 $def[1] .= rrd::gprint('softirq', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
- 
+
 $def[1] .= rrd::area('idle', '#ffffff', 'idle    ', true );
 $def[1] .= rrd::gprint('idle', array('LAST', 'MIN', 'MAX', 'AVERAGE'), '%5.2lf %S' );
 
